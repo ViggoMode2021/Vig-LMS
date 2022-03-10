7a70a848314d49345a961e17ee5b9bcd3efbad95
Ryan Viglione <86791607+ViggoMode2021@users.noreply.github.com>
2022-03-09 19:51:07 -0500

    Add files via upload

--- Vig_LMS.sql
+++ Vig_LMS.sql
@@ -8,7 +8,6 @@ CREATE TABLE users (
 	UNIQUE (email)
 );
 
-
 CREATE TABLE classes(
 	id SERIAL PRIMARY KEY,
 	class_name TEXT,
@@ -19,3 +18,20 @@ CREATE TABLE classes(
 	student_grade INT,
 	class_creator VARCHAR (50) NOT NULL,
 	FOREIGN KEY ("class_creator") REFERENCES users("email") ON DELETE CASCADE);
+
+CREATE TABLE assignments(
+    id SERIAL PRIMARY KEY,
+    assignment_name TEXT NOT NULL,
+    category TEXT NOT NULL,
+    due_date TEXT NOT NULL,
+    overall_points INT NOT NULL,
+	assignment_creator VARCHAR (50) NOT NULL,
+	FOREIGN KEY ("assignment_creator") REFERENCES users("email") ON DELETE CASCADE);
+
+CREATE TABLE assignment_results(
+    id SERIAL PRIMARY KEY,
+    score REAL NOT NULL,
+    student_id INT NOT NULL,
+    assignment_id INT NOT NULL,
+    FOREIGN KEY ("student_id") REFERENCES classes("id") ON DELETE CASCADE,
+    FOREIGN KEY ("assignment_id") REFERENCES assignments("id") ON DELETE CASCADE);
