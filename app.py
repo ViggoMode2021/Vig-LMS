a4ede6ef0c7f519a4e2a54b5f6e767b317591d89
Ryan Viglione <86791607+ViggoMode2021@users.noreply.github.com>
2022-03-31 20:01:02 -0400

    Update app.py

--- app.py
+++ app.py
@@ -405,6 +405,7 @@ def search_attendance_by_date():
          search_attendance_by_date = request.form['search_attendance_by_date']
 
          cursor.execute("""SELECT
+            a.id,
             s.student_first_name,
             s.student_last_name,
             s.class_creator,
@@ -419,6 +420,8 @@ def search_attendance_by_date():
 
          if not search_attendance_by_date:
              flash('Please enter a date according to the format above to view attendance.')
+             cursor.close()
+             conn.close()
              return redirect(url_for('take_attendance_page'))
 
          cursor.close()
@@ -426,6 +429,72 @@ def search_attendance_by_date():
 
          return render_template('search_attendance_by_date.html', search_attendance_query=search_attendance_query, date_object = date_object, account=account, username=session['username'], class_name=session['class_name'],
                                 search_attendance_by_date=search_attendance_by_date)
+
+    return redirect(url_for('login'))
+
+@app.route('/search_attendance_by_student', methods=['POST', 'GET'])
+def search_attendance_by_student():
+
+    if 'loggedin' in session: # Show user and student information from the db
+         conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST)
+
+         cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+
+         cursor.execute('SELECT * FROM users WHERE id = %s', [session['id']])
+         account = cursor.fetchone()
+
+         search_attendance_by_student= request.form['search_attendance_by_student']
+
+         cursor.execute("""SELECT
+            a.id,
+            s.student_first_name,
+            s.student_last_name,
+            s.class_creator,
+            a.date,
+            a.attendance_status
+            FROM classes s
+            INNER JOIN attendance AS a
+            ON a.student_id = s.id
+            WHERE s.id = %s ORDER BY a.date;""", (search_attendance_by_student,))
+
+         search_attendance_query_student = cursor.fetchall()
+
+         cursor.execute("SELECT COUNT (attendance_status) FROM attendance WHERE student_id = %s AND attendance_status = 'Tardy';", (search_attendance_by_student,))
+         student_tardy_count = cursor.fetchall()
+
+         cursor.execute("SELECT COUNT (attendance_status) FROM attendance WHERE student_id = %s AND attendance_status = 'Absent';", (search_attendance_by_student,))
+         student_absent_count = cursor.fetchall()
+
+         cursor.execute("SELECT COUNT (attendance_status) FROM attendance WHERE student_id = %s AND attendance_status = 'Present';", (search_attendance_by_student,))
+         student_present_count = cursor.fetchall()
+
+         if not search_attendance_by_student:
+             flash('Please enter a student id according to the format above to view attendance.')
+             return redirect(url_for('take_attendance_page'))
+
+         cursor.close()
+         conn.close()
+
+         return render_template('search_attendance_by_student.html', search_attendance_query_student=search_attendance_query_student, date_object=date_object, account=account, username=session['username'], class_name=session['class_name'],
+                                search_attendance_by_date=search_attendance_by_date, student_tardy_count=student_tardy_count, student_present_count=student_present_count, student_absent_count=student_absent_count)
+
+    return redirect(url_for('login'))
+
+@app.route('/delete_attendance_record/<string:id>', methods = ['DELETE', 'GET'])
+def delete_attendance_record(id):
+
+    if 'loggedin' in session: # This removes an attendance record from the db.
+
+        conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST)
+        cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+
+        cursor.execute('DELETE FROM attendance WHERE id = {0}'.format(id))
+        conn.commit()
+
+        cursor.close()
+        conn.close()
+
+        return redirect(url_for('take_attendance_page'))
 
     return redirect(url_for('login'))
 
